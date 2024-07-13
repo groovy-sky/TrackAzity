f4e2bfdb882e5f2d5295fe7e8bf6321489fdd443
groovy-sky <groovy-sky>
2024-07-13 12:37:30 +0000

    .

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import os    
 import datetime    
 from azure.mgmt.appcontainers import ContainerAppsAPIClient    
-from azure.servicebus import ServiceBusMessage, ServiceBusClient  
+from azure.servicebus import ServiceBusMessage, ServiceBusClient, ServiceBusReceivedMessage
 from azure.identity import DefaultAzureCredential    
 from time import sleep    
-    
+import json
+import hashlib
+
 class SBClient:  
     def __init__(self, fully_qualified_namespace, queue_name, credential):  
         print("[INF] Initializing Service Bus client")  
+        self.collected_events = []
         self.default_queue = queue_name  
         self.servicebus_client = ServiceBusClient(fully_qualified_namespace, credential)  
   
-    async def send_message(self, message, queue_name=None, content_type="application/json"):  
+    async def send_message(self, message, queue_name=None):  
         print("[INF] Sending a message/-s")  
         queue_name = queue_name or self.default_queue  
         sender = self.servicebus_client.get_queue_sender(queue_name)  
         with sender:  
             if isinstance(message, str):  
-                sender.send_messages([ServiceBusMessage(message,content_type=content_type)])  
+                sender.send_messages(self.genereate_message(message))  
             elif isinstance(message, list):  
                 for item in message:  
-                    self.send_message(item, queue_name, content_type)
+                    self.send_message(item, queue_name)
             else:  
                 raise ValueError("[ERR] Invalid message type. Message must be a string or a list.")  
               
-    def receive_message(self, peek = False, queue_name=None, max_message_count=1):  
+    def receive_message(self, peek = False, queue_name=None, max_message_count=100):  
         print("[INF] Receiving a message/-s")  
         queue_name = queue_name or self.default_queue  
         with self.servicebus_client.get_queue_receiver(queue_name) as receiver:  
@@ -33,6 +36,19 @@ class SBClient:
                 if peek:  
                     receiver.complete_message(message)  
                 return messages  
+            
+    def genereate_message(self, body, subject=None, content_type="application/json"):  
+        if content_type == "application/json":
+            message = json.dumps({"body": body})
+        else:
+            message = body
+        return ServiceBusMessage(message, subject=subject, content_type=content_type)
+    
+    def store_event(self, event):
+        self.collected_events.append({hashlib.md5(event["subject"].lower().encode()).hexdigest(): event})
+
+    def return_events(self):
+        return self.collected_events
   
 def trigger_container_app_job(resource_id,credential):
     print("[INF] Triggering container app job")
@@ -44,14 +60,18 @@ def trigger_container_app_job(resource_id,credential):
         resource_group_name=app_group,    
         job_name=app_name,    
     ).status())  
-  
+
+def parse_message(msg: ServiceBusReceivedMessage):
+    print("[INF] Parsing message")
+    parsed = json.loads(str(msg))
+    result = {'subject': parsed['subject'],'eventType':parsed['eventType'],'action': parsed['data']['authorization']['action']}
+    return result
     
 def main():    
     service_bus_namespace = os.environ.get("SERVICE_BUS_NAMESPACE")    
     queue_name = os.environ.get("SERVICE_BUS_QUEUE_NAME")    
     container_app_job_resource_id = os.environ.get("CONTAINER_APP_JOB_RESOURCE_ID")  
-  
-  
+    
     if not service_bus_namespace or not queue_name or not container_app_job_resource_id:  
         print("[ERR] Missing environment variables")  
         return  
@@ -61,14 +81,18 @@ def main():
     sb_client = SBClient(service_bus_namespace, queue_name, credential)  
   
     start_time = datetime.datetime.now()    
-    end_time = start_time + datetime.timedelta(minutes=3)  # run for 10 minutes    
+    end_time = start_time + datetime.timedelta(minutes=3) 
      
     while datetime.datetime.now() < end_time:      
-        message = sb_client.receive_message(peek = False)    
-        if message:
-            print("[INF] New message in Service Bus has been detected")    
+        messages = sb_client.receive_message(peek = False)    
+        if messages:
+            for message in messages:
+                sb_client.store_event(parse_message(message))
+
+            print(sb_client.return_events())
             trigger_container_app_job(container_app_job_resource_id, credential)
-            return      
-        sleep(30)  # Wait for 1 minute      
+            return
+        sleep(30) 
   
 main()  
+
