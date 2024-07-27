fe5994289ad5340c6c8c29580d10e5e7429a1174
groovy-sky <groovy-sky>
2024-07-27 14:17:02 +0000

    working on table writing

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from azure.identity import DefaultAzureCredential
 from azure.storage.queue import QueueServiceClient,BinaryBase64DecodePolicy
+from azure.data.tables import TableServiceClient,UpdateMode
 from azure.mgmt.resource import ResourceManagementClient
 import os
 import base64
 import json
+import hashlib
 
 class AzClient:
     def __init__(self, subscription_id, credential):
-        print("[INF] Initializing Template Deployer")
+        print("[INF] Initializing Azure REST API Client")
         self.credential = credential
         self.subscription_id = subscription_id
         self.resource_client = ResourceManagementClient(self.credential, self.subscription_id)
@@ -77,13 +79,32 @@ class AzClient:
             if resource.resource_type == resource_type:
                 return resource.api_versions[0]
 
+class TableClient:
+    def __init__(self, account, table, credential):
+        print("[INF] Initializing Azure Table Client")
+        url = "https://" + account + ".table.core.windows.net"
+        self.client = TableServiceClient(endpoint=url, credential=credential).get_table_client(table)
 
+    def upsert(self, entity):
+        print("[INF] Inserting entity into table")
+        self.client.upsert_entity(mode=UpdateMode.MERGE,entity=entity)
+
+    def query(self, query):
+        print("[INF] Querying table")
+        return self.client.query_entities(query)
+    
+    def delete(self, entity):
+        print("[INF] Deleting entity from table")
+        self.client.delete_entity(entity["PartitionKey"], entity["RowKey"])
 
 class QueueClient:
-    def __init__(self, url, credential):
+    def __init__(self, account, credential):
+        print("[INF] Initializing Azure Queue Client")
+        url = "https://" + account + ".queue.core.windows.net"
         self.client = QueueServiceClient(account_url=url, credential=credential,  message_decode_policy=BinaryBase64DecodePolicy())
 
     def receive(self, queue_name, recieve_only = False):
+        print("[INF] Receiving messages from queue")
         response = []
         queue = self.client.get_queue_client(queue_name)
         for message in queue.receive_messages():
@@ -93,20 +114,29 @@ class QueueClient:
         return response
 
     def send(self, queue_name, message):
+        print("[INF] Sending message to queue")
         queue_client = self.client.get_queue_client(queue_name)
         queue_client.send_message(message)
 
 def main():
     storage_name = os.environ.get("STORAGE_NAME")
     queue_name = os.environ.get("QUEUE_NAME") 
-    queue_url = "https://" + storage_name + ".queue.core.windows.net"
+    table_name = os.environ.get("TABLE_NAME")
+
     cred = DefaultAzureCredential()
-    queue = QueueClient(queue_url, cred)
+    queue = QueueClient(storage_name, cred)
+
+    table = TableClient(storage_name, table_name, cred)
+
     default_subscription = "00000000-0000-0000-0000-000000000000"
     az_client = AzClient(default_subscription, cred)
     for item in queue.receive(queue_name):
         peering_info, fail = az_client.get_resource_by_id(json.loads(item).get("subject"),"2024-01-01")
         if peering_info != "" and not fail:  
-            print(peering_info)
+            vnet_info, fail = az_client.get_resource_by_id(peering_info.properties["remoteVirtualNetwork"]["id"],"2024-01-01")
+            if vnet_info != "" and not fail:
+                for ip in vnet_info.properties["addressSpace"]["addressPrefixes"]:
+                    partition = hashlib.sha1(peering_info.properties["remoteVirtualNetwork"]["id"].encode()).hexdigest()
+                    table.upsert({"PartitionKey": partition, "RowKey": hashlib.md5(partition.encode()).hexdigest(), "IP": ip, "peeringState": peering_info.properties["peeringState"]})
 
 main()
