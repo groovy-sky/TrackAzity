771ca4b57754868256892c7325e089acc9847dbe
groovy-sky <groovy-sky>
2024-10-20 12:42:24 +0000

    .

--- azure/script.sh
+++ azure/script.sh
@@ -11,3 +11,15 @@ arm_storage=$(az deployment group create --resource-group $storage_rg --template
 
 topic_name=$(echo $arm_storage | jq -r '.topicName.value')
 storage_account_name=$(echo $arm_storage | jq -r '.storageAccountName.value')
+
+container_location="WestEurope"
+container_name="trackazity-container-rg"
+hub_vnet_id="/subscriptions/20f105d9-0e32-43bb-bd88-3f616a393940/resourceGroups/DvL08g2ULxDxn6/providers/Microsoft.Network/virtualNetworks/vnet-lwxlt98lt28id9"
+devops_org=""
+devops_webhook=""
+
+# Create a resource group
+az group create --name $container_name --location $container_location
+
+# Deploy ARM template for container and related environemnt
+az deployment group create --resource-group $container_name --template-file Container.json --parameters topicName=$topic_name storageAccountName=$storage_account_name 
